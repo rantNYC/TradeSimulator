dbc45f670120e7b06a6f85e7df1e525cd81c22af
RafaelNT <r.ninalayat@outlook.com>
2021-12-26 21:35:58 -0500

    Add dashboard to empty route

--- src/main/javascript/src/App.tsx
+++ src/main/javascript/src/App.tsx
@@ -7,6 +7,7 @@ const App = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Dashboard/>}/>
                 <Route path="/dashboard" element={<Dashboard/>}/>
             </Routes>
         </BrowserRouter>
